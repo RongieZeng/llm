623ded41974d8f36f7f02093e5e0abd2e776cc1d
RongieZeng <bestzrz@gmail.com>
2024-11-24 11:52:04 +0800

    Update qwen-ollama.py
    增加prompt模板

--- qwen-ollama.py
+++ qwen-ollama.py
@@ -15,17 +15,31 @@ def qwen_ollama(prompt):
         "max_tokens": 1024,
         "temperature": 0.7
     }
-    response = requests.post(url, headers=headers, data=json.dumps(data))
+    response = requests.post(url, headers=headers, json = data)
     result = response.json()
     # return result["choices"][0]["message"]["content"]
     return result["choices"][0]["text"]
 
+### 提示模板
 # 情感分析
-prompt = "好好好，您说的都对###请分析一下上面这句话的情绪，并给出一个情感分析结果。使用json格式返回（只返回json结果）,不要输出其他内容。,注意,json的key用英文，不要用中文。json的value可以用中文，格式如下：{\"sentiment\":\"不满\",\"confidence\":0.95}"
-response = qwen_ollama(prompt)
-print(response)
+sentimentPromptTemplate = "%s###请分析一下上面这句话的情绪，并给出一个情感分析结果。输出结果必须被json.loads接受。,注意,json的key用英文，不要用中文。json的value可以用中文，格式如下：{\"sentiment\":\"不满\",\"confidence\":0.95}"
 
 # 实体识别
-prompt = "我在公园上城这里呢，华标说他在公司###请识别上面这句话中的实体，并给出一个实体识别结果。使用json格式返回（只返回json结果）,不要输出其他内容。,注意,json的key用英文.不要用中文.json的value可以用中文，格式严格按下面：{\"entity\":\"实体1\",\"value\":\"实体的值\"}"
-response = qwen_ollama(prompt)
+entityPromptTemplate = "%s###请识别上面这句话中的实体，并给出一个实体识别结果。使用json格式返回（只返回json结果）,不要输出其他内容。,注意,json的key用英文.不要用中文.json的value可以用中文，格式严格按下面：{\"entity\":\"实体1\",\"value\":\"实体的值\"}"
+
+# 摘要
+summaryPromptTemplate = "%s###请对上面这句话进行摘要，并给出一个摘要结果。输出结果必须被json.loads接受。,注意,json的key用英文，不要用中文。json的value可以用中文，格式如下：{\"summary\":\"摘要结果\"}"
+
+### 执行示例
+sentimentPrompt = sentimentPromptTemplate % "好好好，您说的都对"
+response = qwen_ollama(sentimentPrompt)
+print(response)
+
+entityPrompt = entityPromptTemplate % "先生，你好，您现在在哪里啊呀？ 哦，我们现在在五楼啊，您呢？在哪里的五楼？在增城，增城新塘华润公园上城这里哦"
+response = qwen_ollama(entityPrompt)
+print(response)
+
+summaryPrompt = summaryPromptTemplate % "先生，你好，您现在在哪里啊呀？ 哦，我们现在在五楼啊，您呢？在哪里的五楼？在增城，增城新塘华润公园上城这里哦"
+response = qwen_ollama(summaryPrompt)
+
 print(response)
